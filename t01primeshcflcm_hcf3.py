ce1024adcce7aff0b5d5b8d694b3c00182d41565
simtarajade <52946985+simtarajade@users.noreply.github.com>
2019-10-23 13:32:08 +0800

    Update t01primeshcflcm_hcf3.py

--- t01primeshcflcm_hcf3.py
+++ t01primeshcflcm_hcf3.py
@@ -1,5 +1,8 @@
 # Find the highest common factor (HCF) of 3 numbers a, b and c.
-
-a = 
-b = 
-c = 
+q=0
+a = int(input('First number'))
+b = int(input('Second number'))
+c = int(input('Third number'))
+while a%q!=0 and b%q!=0 and c%q!=0:
+  q+=1
+print(q)
